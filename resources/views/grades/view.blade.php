ba0dd0ddef8ba72a2722bc81f9e77c74b2061cd9
OMONDIMICHAEL <mikemike3662@gmail.com>
2025-03-12 21:32:14 +0300

    modified grades.view to allow a route to download insted of preview assignment and added the route to web,php,

--- resources/views/grades/view.blade.php
+++ resources/views/grades/view.blade.php
@@ -39,6 +39,7 @@
                                       <td>{{ $assignment->deadline }}</td>
                                       <td>
                                         <a href="{{ route('download.file', $exam->file_path) }}" class="btn btn-sm btn-info">Download</a>
+                                        <!-- <a href="{{ asset('storage/' . $assignment->file_path) }}" class="btn btn-sm btn-info" target="_blank">Download</a> -->
                                       </td>
                                       <td>
                                           <form action="{{ route('assignments.submit', $assignment->id) }}" method="POST" enctype="multipart/form-data">
@@ -78,6 +79,7 @@
                                       <td>
                                         <!-- this link was for preveiwing in the browser  before downloading -->
                                         <a href="{{ asset('storage/' . $exam->file_path) }}" class="btn btn-sm btn-info" target="_blank">Download</a>
+                                        <!-- <a href="{{ route('exam.download', $exam->id) }}" class="btn btn-sm btn-info">Download</a> -->
                                       </td>
                                       <td>
                                           <form action="{{ route('exams.submit', $exam->id) }}" method="POST" enctype="multipart/form-data">
