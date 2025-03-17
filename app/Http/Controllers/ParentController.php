4d99a0d260553b52c3faa3337f1239d806fd2d13
OMONDIMICHAEL <mikemike3662@gmail.com>
2025-03-17 08:38:58 +0300

    modified parentcontroller added s in model\communication

--- app/Http/Controllers/ParentController.php
+++ app/Http/Controllers/ParentController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Model\Communication;
+use App\Models\Communication;
 use App\Models\User;
 use App\Models\Grade;
 use App\Models\Attendance;
